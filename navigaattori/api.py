d7c190f25352a735e8162b9b5e20313b9aa43bf7
Stefan Hagen <stefan@hagen.link>
2022-12-10 20:39:01 +0100

    More validation of tree specs (still mostly file existence and has content level)

--- navigaattori/api.py
+++ navigaattori/api.py
@@ -32,4 +32,55 @@ def explore(doc_root: str | pathlib.Path, options: dict[str, bool]) -> tuple[int
         log.error(message)
         return 1, message
 
+    spanning_map = structures[STRUCTURES_KEY]
+    if not isinstance(spanning_map, dict):
+        message = f'the ({STRUCTURES_KEY}) key does not provide a map of targets to structure paths'
+        log.error(message)
+        return 1, message
+
+    targets = {
+        t: {
+            'dir': str(pathlib.Path(sp).parent),
+            'file': str(pathlib.Path(sp).name),
+            'structure': {
+                'approvals': None,
+                'bind': None,
+                'changes': None,
+                'meta': None,
+                'render': True,
+                'formats': [],
+                'options': {},
+            },
+            'valid': True,
+        }
+        for t, sp in spanning_map.items()
+    }
+
+    for target, spec in targets.items():
+        spec_path = root / spec['dir'] / spec['file']
+        if not spec_path.is_file() or not spec_path.stat().st_size:
+            log.error(f'spec_path file ({spec_path}) for target ({target}) does not exist or is empty')
+            targets[target]['valid'] = False
+
+    for target, spec in targets.items():
+        if not spec['valid']:
+            log.debug(f'skipping invalid target ({target})')
+            continue
+        spec_path = root / spec['dir'] / spec['file']
+        try:
+            with open(spec_path, 'rt', encoding=ENCODING) as handle:
+                structure = yaml.safe_load(handle)
+            if not structure:
+                log.error(f'structure information for target ({target}) read from file ({spec_path}) is empty')
+                targets[target]['valid'] = False
+        except Exception as ex:
+            log.error(f'reading spec_path file ({spec_path}) for target ({target}) errs with ({ex})')
+            targets[target]['valid'] = False
+
+    log.info(targets)
+    if any(not spec['valid'] for spec in targets.values()):
+        message = f'specifications for targets not all valid'
+        log.error(message)
+        return 1, message
+
     return 0, structures
