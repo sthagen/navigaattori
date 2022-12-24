1e38f7afe4a6adea650c03138c5ee73b51c4c7c9
Stefan Hagen <stefan@hagen.link>
2022-12-24 14:57:02 +0100

    Latest gen license docs hack

--- gen_licenses.py
+++ gen_licenses.py
@@ -23,6 +23,7 @@ fallbacks, FALLBACK_URLS, FALLBACK_AUTHORS, FALLBACK_DESCRIPTIONS = {}, {}, {}, 
 THIRD_PARTY_FALLBACKS = 'third-party-fallbacks.yml'
 TPF_PATH = pathlib.Path(THIRD_PARTY_FALLBACKS)
 if TPF_PATH.is_file():
+    print(f'Reading fallback values from file ({TPF_PATH})', file=sys.stderr)
     with open(TPF_PATH, 'rt', encoding=ENCODING) as handle:
         fallbacks = yaml.safe_load(handle)
 
@@ -39,14 +40,18 @@ indirect_names, INDIRECT_NAMES = [], []
 INDIRECT_PACKAGE_NAMES = 'indirect-package-names.yml'
 IPN_PATH = pathlib.Path(INDIRECT_PACKAGE_NAMES)
 if IPN_PATH.is_file():
+    print(f'Reading indirect names from file ({IPN_PATH})', file=sys.stderr)
     with open(IPN_PATH, 'rt', encoding=ENCODING) as handle:
         indirect_names = yaml.safe_load(handle)
 
 if indirect_names:
+    print(f'Indirect names from file gives map ({indirect_names})', file=sys.stderr)
     if (the_names := indirect_names.get('packages', [])):
-        print(f'extending indirect packages ({the_names}) from file')
+        print(f'Extending indirect packages ({the_names}) from file', file=sys.stderr)
         INDIRECT_NAMES.extend(the_names)
         INDIRECT_NAMES = sorted(set(INDIRECT_NAMES))
+else:
+    print(f'No indirect names from file ({IPN_PATH})', file=sys.stderr)
 
 TARGET = """\
 __version__ = '$version$+parent.$revision$'\
