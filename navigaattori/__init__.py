14849d506df1feb995527681246e330c26dd18d0
Stefan Hagen <stefan@hagen.link>
2023-01-14 22:04:25 +0100

    Bumped impl version for release 2023.1.14

--- navigaattori/__init__.py
+++ navigaattori/__init__.py
@@ -6,8 +6,8 @@ import pathlib
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.1.14+parent.23f56c4b'
+# [[[end]]] (checksum: e6069525e7c4a2bc49db04aa2677ec13)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
